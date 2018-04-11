80ab18b181047f10b383afce8352135463a3f2f7
prebujo <prebujo@gmail.com>
2018-04-11 15:14:12 +0200

    update oblig3

--- Oblig3/Assignment3.m
+++ Oblig3/Assignment3.m
@@ -64,7 +64,17 @@ clearvars -except vehicle_spec call_spec route_spec load_spec V C veh_call N
 %MAIN PROGRAM
 %running the program 10 times with different random seeds each time from
 %41-50
-for j = 1:1
+
+results = zeros(1,16);
+
+%amount of seeds
+s=10;
+
+%timing the run;
+ttime = 0;
+
+
+for j = 1:s
     %choosing a seed for this run.
     rng(40+j);
     
@@ -72,6 +82,10 @@ for j = 1:1
     ini_sol = zeros(1,2*C+V);
     ini_idx = 2*C+V;
     new_sol = zeros(1,2*C+V);
+    
+    n=10000;
+    
+    
 
     %generating initial solution where dummy vehicle has all calls, assuming
     %this solution is always feasible.
@@ -80,33 +94,73 @@ for j = 1:1
         ini_sol(1,ini_idx-1) = i;
         ini_idx = ini_idx - 2;
     end
-    ini_sol = [4 4 0 5 5 7 7 0 3 3 0 1 2 2 6 6 1];
+    %calculating objective for initial solution
+    ini_obj = obj_func(ini_sol,vehicle_spec, route_spec, call_spec, load_spec, V, C, N);
+    %storing initial solution
+    results(1,1) = ini_obj;
     %runnning 10 000 iterations to use random operators on the initial
     %solution, 
-    for i = 1:50
+    tstart = tic;
+    for i = 1:n
         %running the operator function with a random number using randi.
         %always using one of 3 operator functions. Getting a new solution
         %in return that i will then precede to check for feasibility
-        new_sol = operator(2, ini_sol, C, V);
+        new_sol = operator(randi(3), ini_sol, C, V);
+        T0 = 1000000;
+        Tf = 1;
+        T=T0-i*((T0-Tf)/n);
         
         %running feasible check on new solution.
         feas = feas_check(new_sol, veh_call, vehicle_spec, call_spec, route_spec, load_spec, C, V, N);
         %if feasible, update initial solution
         if(feas)
-            ini_sol = new_sol;
+            new_obj = obj_func(new_sol, vehicle_spec, route_spec, call_spec, load_spec, V, C, N);
+            if(new_obj < ini_obj)
+                ini_obj = new_obj;
+                ini_sol = new_sol;
+            else
+                x = rand;
+                p = exp((-(new_obj-ini_obj)/T));
+                if(rand<p)
+                    ini_sol = new_sol;
+                    ini_obj = new_obj;
+                end
+            end
         end        
     end
+    ttime = ttime + toc(tstart);
+    %storing result..
+    results(1,j+1) = ini_obj;
+    
+    
 end
 
+%storing sum for average..
+results(1,2+s) = results(1,2+s)+ini_obj;
 
+%calculating average..
+results(1,2+s) = sum(results(1,2:s+1))/10;
 
+%storing average improvement
+a(1,1:s) = results(1,1);
+b = results(1,2:s+1);
+results(1,3+s) = ((a-b)/a)*100;
+
+%storing best run
+results(1,4+s) = min(results(1,2:s+1));
+
+%storing best improvement
+results(1,5+s) = (results(1,1)-results(1,4+s))/results(1,1)*100;
+
+%storing average time
+results(1,6+s) = ttime/s;
 
 
     
     
     
     
-clearvars -except vehicle_spec call_spec route_spec load_spec V C veh_call N ini_sol
+clearvars -except vehicle_spec call_spec route_spec load_spec V C veh_call N ini_sol ini_obj results a b c d
 
 %END OF MAIN PROGRAM
 %__________________________________________________________________________
@@ -120,9 +174,11 @@ function o = operator(operator, ini_sol, C,V)
         case 1
             new_sol = operator1(ini_sol, C, V);
         case 2
-            new_sol = operator2(ini_sol, C, V);
-        case 3
             new_sol = operator3(ini_sol, C, V);
+        case 3
+            new_sol = operator4(ini_sol, C, V);
+        case 4
+            new_sol = operator5(ini_sol, C, V);
         otherwise
             new_sol = 0;
     end
@@ -137,13 +193,14 @@ function o1 = operator1(ini_sol, C, V)
     idx = 2*C + V;
     call_choice = 0;
     
+    %choosing randomly a non zero call from the initial solution
     while(call_choice==0)
-        call_choice = ini_sol(1,randi(2*C+V));
+        call_choice = ini_sol(1,randi(idx));
     end
     
     %the following section finds the vehicle belonging to the chosen call
     veh_choice = 1;
-    for i = 1:2*C+V
+    for i = 1:idx
         call = ini_sol(1,i);
         if(call == call_choice)
             break;
@@ -168,12 +225,16 @@ function o1 = operator1(ini_sol, C, V)
     new_idx = 1;
     curr_veh = 1;
     opt_idx = 1;
-    opt = zeros(1,C);
+    opt = zeros(1,2);
     opt(1) = call_choice;
     i = 1;
-    while(i<=2*C+V)
+    while(i<=idx||new_idx<=idx)
         if(curr_veh == new_veh)
-            call = ini_sol(1,i);
+            if(i>idx)
+                call = 0;
+            else
+                call = ini_sol(1,i);
+            end
             if(call == 0) %if the vehicle has no other calls,
                 new_sol(1,new_idx) = call_choice; % I will set the vehicle
                 new_sol(1,new_idx+1) = call_choice; %to pick up and deliver
@@ -202,7 +263,7 @@ function o1 = operator1(ini_sol, C, V)
                         end
                     else
                         i = i+1;
-                        if(i>2*C+V)
+                        if(i>idx)
                             opt_idx = opt_idx-1;
                             opt(2) = 0;                      
                         elseif(ini_sol(1,i)==0)
@@ -230,8 +291,6 @@ function o1 = operator1(ini_sol, C, V)
             end
         end        
     end
-    disp(ini_sol);
-    disp(new_sol);
     o1=new_sol;
 end
 
@@ -288,14 +347,298 @@ function o2 = operator2(ini_sol, C, V)
         i = i+1;        
    end    
     
-    disp(ini_sol);
-    disp(new_sol);
     o2=new_sol;
 end
 
 
 
 %__________________________________________________________________________
+
+%__________________________________________________________________________
+%OPERATOR 3
+%Operator to make a 3-exchange for a vehicle with at least two 
+%pickup/deliveries
+
+function o3 = operator3(ini_sol, C, V)
+    %not including the dummy vehicle as it wont make any difference
+    %choosing randomly a vehicle.
+    veh_choice = randi(V);
+    veh = 1;
+    %the following section finds the vehicle belonging to the chosen call
+    i = 1;
+    %generating the new solution
+    new_sol = ini_sol;
+    while(i<2*C+V)
+        call = ini_sol(1,i);
+        if(veh == veh_choice)
+            %remembering starting index
+            idx = i-1;
+            %counting how many pickups/deliveries i have
+            count = 0;
+            while(call~=0)
+               	count = count + 1;               
+                i = i+1;
+                if(i>2*C+V)
+                    break;
+                end
+                call = ini_sol(1,i);
+            end
+            %if vehicle has some calls
+            if(count > 2)
+                choice1 = randi(count);
+                choice2 = choice1;
+                while(choice2 == choice1)
+                    choice2 = randi(count);
+                end
+                choice3 = choice2;
+                while(choice3 ==choice2 || choice3 == choice1)
+                    choice3 = randi(count);
+                end
+                choice1 = choice1 + idx;
+                choice2 = choice2 + idx;
+                choice3 = choice3 + idx;
+                %i swap one random choice with another
+                new_sol(1,choice1) = ini_sol(1,choice2);
+                new_sol(1,choice2) = ini_sol(1,choice3);
+                new_sol(1,choice3) = ini_sol(1,choice1);
+            end
+            %done with swap, breaking out
+            break;
+        elseif(call == 0)
+            veh = veh +1;
+        end
+        i = i+1;        
+   end    
+    o3=new_sol;
+end
+%__________________________________________________________________________
+
+%__________________________________________________________________________
+%OPERATOR 4
+%Operator to switch the pickup and delivery of one random call with 
+%another random call.
+
+function o4 = operator4(ini_sol, C, V)
+    
+    %choosing random calls to change, want always different calls
+    call_choice1 = 0;
+    call_choice2 = 0;
+    
+    while(call_choice1==0)
+        call_choice1 = ini_sol(1,randi(2*C+V));
+    end
+    
+    while(call_choice2 == 0 || call_choice2 == call_choice1)
+        call_choice2 = ini_sol(1,randi(2*C+V));
+    end
+    
+
+    %the following section changes the calls in the new solution vector
+    new_sol = ini_sol;
+    for i = 1:2*C+V
+        call = ini_sol(1,i);
+        if(call == call_choice1)
+            new_sol(1,i) = call_choice2;
+        elseif(call == call_choice2)
+            new_sol(1,i) = call_choice1;
+        end
+    end
+    
+    o4=new_sol;
+end
+
+
+
+%__________________________________________________________________________
+
+%__________________________________________________________________________
+%OPERATOR 5
+%Operator to move one random call from one vehicle to another random 
+%vehicle. Operator is behaving backwards in comparison with operator 1, ie.
+%it starts the selection of calls at the end of the delivery schedule
+%instead of the beginning.
+
+function o5 = operator5(ini_sol, C, V)
+    idx = 2*C + V;
+    call_choice = 0;
+    
+    while(call_choice==0)
+        call_choice = ini_sol(1,randi(2*C+V));
+    end
+    
+    %the following section finds the vehicle belonging to the chosen call
+    veh_choice = 1;
+    for i = 1:idx
+        call = ini_sol(1,i);
+        if(call == call_choice)
+            break;
+        elseif(call == 0)
+            veh_choice = veh_choice +1;
+        end        
+    end
+    
+    %As all calls are assigned to dummy in initial solution I am not
+    %including dummy vehicle as an option here. This might lock possible
+    %solutions if i dont include other operators to move a route back to
+    %random, I will consider changing this. Since I could theoretically end up
+    %picking a call from a vehicle and moving it to itsself i am doing a 
+    %while loop until that is not the case. tatt med dummy også..
+    new_veh = veh_choice;
+    while(new_veh == veh_choice)
+        new_veh = randi(V+1);
+    end
+    
+    %generating new solution vector
+    new_sol = zeros(1,idx);
+    new_idx = idx;
+    curr_veh = V+1;
+    opt_idx = 1;
+    opt = zeros(1,2);
+    opt(1) = call_choice;
+    i = idx;
+    while(i>0||new_idx>0)
+        if(curr_veh == new_veh)          
+            if(i<1)
+                call = 0;
+            else
+                call = ini_sol(1,i);
+            end
+            if(call == 0) %if the vehicle has no other calls,
+                new_sol(1,new_idx) = call_choice; % I will set the vehicle
+                new_sol(1,new_idx-1) = call_choice; %to pick up and deliver
+                new_idx = new_idx -3;  %the chosen call.
+                i = i -1; %and continue the loop through the initial solution
+                opt(1) = 0;
+                opt_idx = opt_idx-1;
+            else
+                opt_idx = opt_idx+1;
+                opt(opt_idx) = call;
+                choice_pickedup = 0;
+                while(opt_idx >0)
+                    choice = opt(1,randi(opt_idx));
+                    %0 is only possbile when I am done with chosen call
+                    if(choice == 0)
+                        choice = opt(2);
+                    end
+                    new_sol(1,new_idx) = choice;
+                    new_idx = new_idx-1;
+                    if(choice == call_choice)                 
+                        if(choice_pickedup)
+                            opt(1) = 0;
+                            opt_idx = opt_idx-1;
+                        else
+                            choice_pickedup = 1;
+                        end
+                    else
+                        i = i-1;
+                        if(i<1)
+                            opt_idx = opt_idx-1;
+                            opt(2) = 0;                      
+                        elseif(ini_sol(1,i)==0)
+                            opt_idx = opt_idx -1;
+                            opt(2)=0;
+                        else
+                            opt(2) = ini_sol(1,i);
+                        end
+                    end
+                end
+            end
+            curr_veh = curr_veh-1;            
+        else
+            call = ini_sol(1,i);
+            if(call == call_choice)
+                i = i-1;
+            elseif(call==0)
+                curr_veh = curr_veh -1;
+                i = i-1;
+                new_idx = new_idx-1;
+            else
+                new_sol(1,new_idx) = ini_sol(1,i);
+                i = i-1;
+                new_idx = new_idx-1;
+            end
+        end        
+    end
+    o5=new_sol;
+end
+
+
+
+%__________________________________________________________________________
+
+%__________________________________________________________________________
+%OPERATOR 6
+%Operator to move one random call from one vehicle to another random 
+%vehicle. Operator puts pickup and delivery in the end of the vehicles
+%schedule.
+
+function o6 = operator6(ini_sol, C, V)
+    idx = 2*C + V;
+    call_choice = 0;
+    
+    while(call_choice==0)
+        call_choice = ini_sol(1,randi(2*C+V));
+    end
+    
+    %the following section finds the vehicle belonging to the chosen call
+    veh_choice = 1;
+    for i = 1:2*C+V
+        call = ini_sol(1,i);
+        if(call == call_choice)
+            break;
+        elseif(call == 0)
+            veh_choice = veh_choice +1;
+        end        
+    end
+    
+    %As all calls are assigned to dummy in initial solution I am not
+    %including dummy vehicle as an option here. This might lock possible
+    %solutions if i dont include other operators to move a route back to
+    %random, I will consider changing this. Since I could theoretically end up
+    %picking a call from a vehicle and moving it to itsself i am doing a 
+    %while loop until that is not the case. tatt med dummy også..
+    new_veh = veh_choice;
+    while(new_veh == veh_choice)
+        new_veh = randi(V+1);
+    end
+    
+    %generating new solution vector
+    new_sol = zeros(1,idx);
+    new_idx = 1;
+    curr_veh = 1;
+    i = 1;
+    while(i<=idx || new_idx<=idx)
+        if(i>2*C+V)
+            call = 0;
+        else
+            call = ini_sol(1,i);
+        end
+        %if I am at the end of the new vehicles schedule
+        if(call == 0 && curr_veh == new_veh) 
+            new_sol(1,new_idx) = call_choice; % I will set the vehicle
+            new_sol(1,new_idx+1) = call_choice; %to pick up and deliver
+            new_idx = new_idx +3;  %the chosen call.
+            i = i +1; %and continue the loop through the initial solution
+            curr_veh = curr_veh+1;
+        elseif(call == 0)
+            curr_veh = curr_veh+1;
+            i = i+1;
+            new_idx = new_idx+1;
+        elseif(call == call_choice)
+            i = i+1;
+        else
+            new_sol(1,new_idx) = call;
+            new_idx = new_idx+1;
+            i = i+1;
+        end
+    end
+    o6=new_sol;
+end
+
+
+
+%__________________________________________________________________________
+
 
 %__________________________________________________________________________
 %FEASIBILITY CHECKS
@@ -305,17 +648,14 @@ end
 function f = feas_check(route, veh_call, vehicle_spec, call_spec, route_spec, load_spec, C, V, N)
     feasible = 1;
         if(~feas_check1(route, veh_call, C, V))
-            disp("Feas check 1 not passed");
             feasible = 0;
         end
         
         if(~feas_check2(route, vehicle_spec, call_spec, C,V))
-            disp("Feas check 2 not passed");
             feasible = 0;
         end
         
         if(~feas_check3(route, vehicle_spec, call_spec, route_spec, load_spec, C, V, N))
-            disp("Feas check 3 not passed");
             feasible = 0;
         end
     f = feasible;
@@ -479,7 +819,7 @@ function of = obj_func(route, vehicle_spec, route_spec, call_spec, load_spec, V,
         call = route(1, j);        
         if(call == 0)
             veh = veh + 1;
-            if(veh == 6)
+            if(veh == V+1)
                 idx = j+1;
                 break;
             end
